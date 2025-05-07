e71e030bab9f5fb84401d0409cc6c60e73beba5b
tsach <tsachbak9@gmail.com>
2025-05-07 20:54:18 +0300

    Add navigation bar (desktop and mobile).

---
+++ script.js
@@ -0,0 +1,9 @@
+//Menu
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menuBtn");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+});
