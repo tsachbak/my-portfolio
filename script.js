f7dfb35e8534accae2760f3e65d823c6b4adf404
tsach <tsachbak9@gmail.com>
2025-05-08 11:44:08 +0300

    update skills and UI in About section.

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ document.addEventListener("DOMContentLoaded", () => {
     mobileMenu.classList.toggle("hidden");
   });
 });
+
+//My Tech Skills
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("skillsBtn");
+  const skillsPopUp = document.getElementById("skillsPopup");
+  const closeBtn = document.getElementById("closePopUp");
+
+  btn.addEventListener("click", () => {
+    skillsPopUp.classList.remove("hidden");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    skillsPopUp.classList.add("hidden");
+  });
+
+  document.addEventListener("click", (event) => {
+    const isClickInside =
+      skillsPopUp.contains(event.target) || btn.contains(event.target);
+    if (!isClickInside) {
+      skillsPopUp.classList.add("hidden");
+    }
+  });
+});
